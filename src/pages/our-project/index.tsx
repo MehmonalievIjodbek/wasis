7cfe734165af49c9fef000e3e0ac03fd3bba4ea9
MehmonalievIjodbek <mehmonaliyevijodbek@gmail.com>
2023-06-06 14:39:38 +0500

    'as

--- src/pages/our-project/index.tsx
+++ src/pages/our-project/index.tsx
@@ -22,42 +22,42 @@ export default function index() {
     {
       id: 1,
       title: 'project-title1',
-      text: '1998 - 2002',
+      // text: '1998 - 2002',
       desc: 'project-desc1',
       imd: '/assets/images/1.jpg',
     },
     {
       id: 2,
       title: 'project-title2',
-      text: '2002 - 2004',
+      // text: '2002 - 2004',
       desc: 'project-desc2',
       imd: '/assets/images/4.jpg',
     },
     {
       id: 3,
       title: 'project-title3',
-      text: '2004 - 2005',
+      // text: '2004 - 2005',
       desc: 'project-desc3',
       imd: '/assets/images/3.jpg',
     },
     {
       id: 4,
       title: 'project-title4',
-      text: '2005 - 2010',
+      // text: '2005 - 2010',
       desc: 'project-desc4',
       imd: '/assets/images/2.jpg',
     },
     {
       id: 5,
       title: 'project-title5',
-      text: '2010 - 2013',
+      // text: '2010 - 2013',
       desc: 'project-desc5',
       imd: '/assets/images/5.jpg',
     },
     {
       id: 6,
       title: 'project-title6',
-      text: '2013 - 2017',
+      // text: '2013 - 2017',
       desc: 'project-desc6',
       imd: '/assets/images/6.jpg',
     },
@@ -132,7 +132,7 @@ export default function index() {
                     <>
                       <div key={project.id} className="chemicals__left-card">
                         <h5>{parse(t(project.title, { ns: 'common' }))}</h5>
-                        <p>{t(project.text, { ns: 'common' })}</p>
+                        {/* <p>{t(project.text, { ns: 'common' })}</p> */}
                         <h6>{parse(t(project.desc, { ns: 'common' }))}</h6>
                         <div className="chemicals-button__card">
                           <button
